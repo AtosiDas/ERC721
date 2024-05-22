2e4f48402dd937adcd713a08ea58aab02148e45e
Atosi <atosidas44@gmail.com>
2024-05-22 13:53:45 +0530

    Update the code for royalties

--- second.sol
+++ second.sol
@@ -3,22 +3,68 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    struct RoyaltyInfo {
+        address creator;
+        uint256 royaltyPercentage; // value will be 0 to 100
+    }
+
     address[] public shareHolders;
     uint256[] share;
+
     mapping(address => uint256) shares;
     mapping(address => bool) status;
+    mapping(uint256 => RoyaltyInfo) private _royalties;
+
+    event NFTMinted(
+        uint256 tokenId,
+        address creator,
+        uint256 royaltyPercentage
+    );
+    event NFTSold(
+        uint256 tokenId,
+        address seller,
+        address buyer,
+        uint256 salePrice
+    );
+    event NFTRoyaltiesPaid(
+        uint256 tokenId,
+        address creator,
+        uint256 royaltyAmount
+    );
+
     constructor() ERC721("Unique Graffiti tokens", "UGT") {
         shareHolders.push(msg.sender);
         shares[msg.sender] = 100;
         status[msg.sender] = true;
     }
+
     uint256 public price = 0.02 ether;
-    function mint(uint256 _id, string memory _uri) external payable{
-        require(msg.value == price, "Insufficient balance!");
-        _mint(msg.sender, _id);
-        _setTokenURI(_id, _uri);
+
+    function mintNFT(
+        string memory tokenURI,
+        uint256 royaltyPercentage
+    ) public payable returns (uint256) {
+        require(royaltyPercentage <= 100, "Royalty percentage too high");
+        require(msg.value == price);
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _royalties[newItemId] = RoyaltyInfo({
+            creator: msg.sender,
+            royaltyPercentage: royaltyPercentage
+        });
+
+        emit NFTMinted(newItemId, msg.sender, royaltyPercentage);
+        return newItemId;
     }
 
     function changePrice(uint256 _price) external onlyOwner {
@@ -30,16 +76,18 @@ contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
         _burn(id);
     }
 
-    function withdraw() public onlyOwner {
-        payable (msg.sender).transfer(address(this).balance);
-    }
+    // function withdraw() public onlyOwner {
+    //     payable (msg.sender).transfer(address(this).balance);
+    // }
+
     function checkShare(uint256 _share) internal view {
         uint256 sum;
-        for(uint256 i = 0; i < share.length; i++){
+        for (uint256 i = 0; i < share.length; i++) {
             sum += share[i];
         }
-        require(sum + _share <= 100,"share must be under 100");
+        require(sum + _share <= 100, "share must be under 100");
     }
+
     function addShareHolders(address _addr, uint256 _share) public onlyOwner {
         checkShare(_share);
         require(status[_addr] == false, "Address is already a shareholder.");
@@ -49,11 +97,35 @@ contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
         shares[_addr] = _share;
         shares[msg.sender] -= _share;
     }
-    function sharedWithdrawl() public onlyOwner payable{
+
+    function sharedWithdrawl() public payable onlyOwner {
         uint256 balance = address(this).balance;
-        for(uint256 i = 0; i < shareHolders.length; i++){
-            payable(shareHolders[i]).transfer((shares[shareHolders[i]]*balance)/100);
+        for (uint256 i = 0; i < shareHolders.length; i++) {
+            payable(shareHolders[i]).transfer(
+                (shares[shareHolders[i]] * balance) / 100
+            );
         }
     }
-}
 
+    function buyNFT(uint256 tokenId) external payable {
+        address seller = ownerOf(tokenId);
+        require(seller != msg.sender, "Cannot buy your own NFT");
+        require(msg.value >= price, "Atleast send 0.02 ether");
+
+        // Transfer the NFT to the buyer
+        _transfer(seller, msg.sender, tokenId);
+
+        RoyaltyInfo memory royalty = _royalties[tokenId];
+        uint256 royaltyAmount = (msg.value * royalty.royaltyPercentage) / 100;
+
+        // Send sale proceeds to the seller minus royalty
+        uint256 sellerAmount = msg.value - royaltyAmount;
+        payable(seller).transfer(sellerAmount);
+
+        // Send royalty to the creator
+        payable(royalty.creator).transfer(royaltyAmount);
+
+        emit NFTSold(tokenId, seller, msg.sender, msg.value);
+        emit NFTRoyaltiesPaid(tokenId, royalty.creator, royaltyAmount);
+    }
+}
