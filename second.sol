0867a1e129b12bb724f5a9fbd5a09e6a20fdeb72
Atosi <atosidas44@gmail.com>
2024-05-22 14:36:04 +0530

    Update the code so that owner can update the price for a particular NFT

--- second.sol
+++ second.sol
@@ -7,8 +7,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    // using Counters for Counters.Counter;
+    // Counters.Counter private _tokenIds;
+    uint256 public defaultPrice;
 
     struct RoyaltyInfo {
         address creator;
@@ -21,6 +22,7 @@ contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
     mapping(address => uint256) shares;
     mapping(address => bool) status;
     mapping(uint256 => RoyaltyInfo) private _royalties;
+    mapping(uint256 => uint256) TokenPrice;
 
     event NFTMinted(
         uint256 tokenId,
@@ -38,21 +40,21 @@ contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
         address creator,
         uint256 royaltyAmount
     );
+    event PriceChange(uint256 tokenID, uint256 price);
 
-    constructor() ERC721("Unique Graffiti tokens", "UGT") {
+    constructor(uint256 _price) ERC721("Unique Graffiti tokens", "UGT") {
         shareHolders.push(msg.sender);
         shares[msg.sender] = 100;
         status[msg.sender] = true;
+        defaultPrice = _price;
     }
-
-    uint256 public price = 0.02 ether;
 
     function mintNFT(
         uint256 tokenID,
         uint256 royaltyPercentage
     ) public payable {
         require(royaltyPercentage <= 100, "Royalty percentage too high");
-        require(msg.value == price);
+        require(msg.value == defaultPrice);
         //_tokenIds.increment();
         // uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, tokenID);
@@ -62,12 +64,12 @@ contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
             creator: msg.sender,
             royaltyPercentage: royaltyPercentage
         });
-
+        TokenPrice[tokenID] = defaultPrice;
         emit NFTMinted(tokenID, msg.sender, royaltyPercentage);
     }
 
     function changePrice(uint256 _price) external onlyOwner {
-        price = _price;
+        defaultPrice = _price;
     }
 
     function burn(uint256 id) external {
@@ -109,7 +111,10 @@ contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
     function buyNFT(uint256 tokenId) external payable {
         address seller = ownerOf(tokenId);
         require(seller != msg.sender, "Cannot buy your own NFT");
-        require(msg.value >= price, "Atleast send 0.02 ether");
+        require(
+            msg.value >= TokenPrice[tokenId],
+            "Atleast send the token price."
+        );
 
         // Transfer the NFT to the buyer
         _transfer(seller, msg.sender, tokenId);
@@ -126,5 +131,11 @@ contract MyNFT is ERC721URIStorage, Ownable(msg.sender) {
 
         emit NFTSold(tokenId, seller, msg.sender, msg.value);
         emit NFTRoyaltiesPaid(tokenId, royalty.creator, royaltyAmount);
+    }
+
+    function changePriceOfNFT(uint256 _tokenID, uint256 _price) public {
+        require(ownerOf(_tokenID) == msg.sender, "Yor are not eligible.");
+        TokenPrice[_tokenID] = _price;
+        emit PriceChange(_tokenID, _price);
     }
 }
